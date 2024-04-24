9d0f38c863ef8033b16dd9b90130ed7e500bcf5d
omerfaruksomay <omerfaruksomay002@gmail.com>
2024-04-24 11:05:06 +0300

    report section created

--- resources/views/report/index.blade.php
+++ resources/views/report/index.blade.php
@@ -14,81 +14,41 @@
                 @endif
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="/home">Main Functions</a></li>
+                        <li class="breadcrumb-item"><a href="/dashboard">Main Functions</a></li>
                         <li class="breadcrumb-item"><a href="/report">Report</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">Result</li>
                     </ol>
                 </nav>
             </div>
         </div>
-        {{-- <div class="row">
-            <div class="col-md-12">
-                @if ($sales->count() > 0)
-                    <div class="alert alert-success" role="alert">
-                        <p>The Total Amount of Sale from {{ $dateStart }} to {{ $dateEnd }} is
-                            ${{ number_format($totalSale, 2) }}</p>
-                        <p>Total Result: {{ $sales->total() }}</p>
-                    </div>
-                    <table class="table">
-                        <thead>
-                            <tr class="bg-primary text-light">
-                                <th scope="col">#</th>
-                                <th scope="col">Receipt ID</th>
-                                <th scope="col">Date Time</th>
-                                <th scope="col">Table</th>
-                                <th scope="col">Staff</th>
-                                <th scope="col">Total Amount</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @php
-                                $countSale = ($sales->currentPage() - 1) * $sales->perPage() + 1;
-                            @endphp
-                            @foreach ($sales as $sale)
-                                <tr class="bg-primary text-light">
-                                    <td>{{ $countSale++ }}</td>
-                                    <td>{{ $sale->id }}</td>
-                                    <td>{{ date('m/d/Y H:i:s', strtotime($sale->updated_at)) }}</td>
-                                    <td>{{ $sale->table_name }}</td>
-                                    <td>{{ $sale->user_name }}</td>
-                                    <td>{{ $sale->total_price }}</td>
-                                </tr>
-                                <tr>
-                                    <th></th>
-                                    <th>Menu ID</th>
-                                    <th>Menu</th>
-                                    <th>Quantity</th>
-                                    <th>Price</th>
-                                    <th>Total Price</th>
-                                </tr>
-                                @foreach ($sale->saleDetails as $saleDetail)
-                                    <tr>
-                                        <td></td>
-                                        <td>{{ $saleDetail->menu_id }}</td>
-                                        <td>{{ $saleDetail->menu_name }}</td>
-                                        <td>{{ $saleDetail->quantity }}</td>
-                                        <td>{{ $saleDetail->menu_price }}</td>
-                                        <td>{{ $saleDetail->menu_price * $saleDetail->quantity }}</td>
-                                    </tr>
-                                @endforeach
-                            @endforeach
-                        </tbody>
-                    </table>
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Table name</th>
+                    <th scope="col">User name</th>
+                    <th scope="col">Total price</th>
+                    <th scope="col">Total recieved</th>
+                    <th scope="col">Change</th>
+                    <th scope="col">Payment type</th>
 
-                    {{ $sales->appends($_GET)->links() }}
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($sales as $sale)
+                    <tr>
+                        <td scope="row">{{ $sale->id }}</td>
+                        <td scope="row">{{ $sale->table_name }}</td>
+                        <td scope="row">{{ $sale->user_name }}</td>
+                        <td scope="row">{{ $sale->total_price }}</td>
+                        <td scope="row">{{ $sale->total_recieved }}</td>
+                        <td scope="row">{{ $sale->change }}</td>
+                        <td scope="row">{{ $sale->payment_type }}</td>
 
-                    <form action="/report/show/export" method="get">
-                        <input type="hidden" name="dateStart" value="{{ $dateStart }}">
-                        <input type="hidden" name="dateEnd" value="{{ $dateEnd }}">
-                        <input type="submit" class="btn btn-warning" value="Export to Excel">
-                    </form>
-                @else
-                    <div class="alert alert-danger" role="alert">
-                        There is no Sale Report
-                    </div>
-                @endif
-            </div>
-        </div> --}}
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
     </div>
 
 </x-app-layout>
