8134cebd706c4e375c264c4dbe470e423d7b4892
omerfaruksomay <omerfaruksomay002@gmail.com>
2024-06-13 14:06:44 +0300

    reports page bugs fixed

--- app/Http/Controllers/Report/ReportController.php
+++ app/Http/Controllers/Report/ReportController.php
@@ -10,7 +10,7 @@ class ReportController extends Controller
 {
     public function index()
     {
-        $sales = Sale::where('sale_status', 'paid');
+        $sales = Sale::all();
         return view('report.index')->with('sales', $sales);
     }
     // public function show(Request $request)
